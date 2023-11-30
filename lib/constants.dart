f0b3c5993397bf35259895ae97a9515ffed16f31
Sai Kiran Katayath <saikiran@Sais-MacBook-Pro.local>
2023-12-01 00:03:56 +0530

    added home page,beds UI, change keys UI along with bottom sheet

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 Color kPrimaryColor = Colors.purple.shade900;
+const Color kWhiteColor = Colors.white;
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hostel_management_user/constants.dart';
+import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,10 +10,65 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor.withOpacity(0.8),
+        title: Text(
+          'Garage X',
+          style: GoogleFonts.poppins(color: kWhiteColor),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Iconsax.user),
+            color: kWhiteColor,
+          ),
+          const SizedBox(width: 15.0)
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Container(
+              width: Get.width,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6),
+              decoration: BoxDecoration(
+                  color: kPrimaryColor.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(8.0)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Text(
+                      'Keys are at Dining',
+                      style: GoogleFonts.poppins(
+                          color: kPrimaryColor, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  IconButton(
+                      padding: EdgeInsets.zero,
+                      tooltip: 'Change keys status',
+                      onPressed: () {
+                        Get.bottomSheet(const UpdateKeyStateSheet());
+                      },
+                      icon: Icon(
+                        Iconsax.repeat5,
+                        color: kPrimaryColor,
+                      ))
+                ],
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            Text(
+              'Beds',
+              style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w500,
+                  color: kPrimaryColor,
+                  fontSize: 21.0),
+            ),
+            const SizedBox(height: 12.0),
             Expanded(
               child: GridView.builder(
                   shrinkWrap: true,
@@ -19,7 +76,7 @@ class HomeScreen extends StatelessWidget {
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisSpacing: 16.0,
                       mainAxisSpacing: 16.0,
-                      childAspectRatio: 0.7,
+                      childAspectRatio: 0.75,
                       crossAxisCount: 2),
                   itemBuilder: (conetext, index) {
                     return Container(
@@ -39,6 +96,113 @@ class HomeScreen extends StatelessWidget {
                     );
                   }),
             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class UpdateKeyStateSheet extends StatelessWidget {
+  const UpdateKeyStateSheet({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        color: kWhiteColor,
+      ),
+      constraints: BoxConstraints(
+        maxHeight: Get.height * 0.6,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Change Keys \nStatus',
+              style: GoogleFonts.poppins(color: kPrimaryColor, fontSize: 32.0),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: kPrimaryColor, width: 2),
+                  color: kPrimaryColor.withOpacity(0.2),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(12.0)),
+              child: ListTile(
+                title: Text(
+                  'Dining',
+                  style: GoogleFonts.poppins(color: kPrimaryColor),
+                ),
+                trailing: const Icon(Icons.done),
+                tileColor: kPrimaryColor,
+                enabled: true,
+                selectedTileColor: kPrimaryColor,
+                selectedColor: kPrimaryColor,
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            Container(
+              decoration: BoxDecoration(
+                  // border: Border.all(color: kPrimaryColor, width: 2),
+                  color: kPrimaryColor.withOpacity(0.1),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(
+                    12.0,
+                  )),
+              child: ListTile(
+                title: Text(
+                  'Open',
+                  style: GoogleFonts.poppins(color: kPrimaryColor),
+                ),
+                // trailing: const Icon(Icons.done),
+                tileColor: kPrimaryColor,
+                enabled: true,
+                selectedTileColor: kPrimaryColor,
+                selectedColor: kPrimaryColor,
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            Container(
+              decoration: BoxDecoration(
+                  // border: Border.all(color: kPrimaryColor, width: 2),
+                  color: kPrimaryColor.withOpacity(0.1),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(
+                    12.0,
+                  )),
+              child: ListTile(
+                title: Text(
+                  'I\'ve the Keys',
+                  style: GoogleFonts.poppins(color: kPrimaryColor),
+                ),
+                // trailing: const Icon(Icons.done),
+                tileColor: kPrimaryColor,
+                enabled: true,
+                selectedTileColor: kPrimaryColor,
+                selectedColor: kPrimaryColor,
+              ),
+            ),
+            const SizedBox(height: 32.0),
+            Center(
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0)),
+                      backgroundColor: kPrimaryColor.withOpacity(0.8),
+                      fixedSize: Size(Get.width * 0.9, 56.0)),
+                  child: Text(
+                    'Update',
+                    style: GoogleFonts.karla(color: kWhiteColor),
+                  )),
+            ),
+            const SizedBox(height: 24.0),
           ],
         ),
       ),
